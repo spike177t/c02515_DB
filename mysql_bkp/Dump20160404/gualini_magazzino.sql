c05bc6709ec0039669758c6bf3cc649a6be112fe
d.suffritti <d.suffritti@comarautomazioni.it>
2016-04-04 14:39:46 +0200

    Dump include Create schema

---
+++ mysql_bkp/Dump20160404/gualini_magazzino.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `gualini` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gualini`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gualini
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `magazzino`
+--
+
+DROP TABLE IF EXISTS `magazzino`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `magazzino` (
+  `ID_MAGAZZINO` int(11) NOT NULL,
+  `NOME` varchar(45) DEFAULT NULL,
+  `STATO` varchar(45) DEFAULT NULL COMMENT 'indica se il magazzimo è: 0 = non utilizzare; 1 = disponibile',
+  `PIENO` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Indica se il magazziono è pieno o se vi sono cassetti disponibili',
+  PRIMARY KEY (`ID_MAGAZZINO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `magazzino`
+--
+
+LOCK TABLES `magazzino` WRITE;
+/*!40000 ALTER TABLE `magazzino` DISABLE KEYS */;
+INSERT INTO `magazzino` VALUES (1,'MAG_1','0',0),(2,'MAG_2','0',0);
+/*!40000 ALTER TABLE `magazzino` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-04 14:39:15
