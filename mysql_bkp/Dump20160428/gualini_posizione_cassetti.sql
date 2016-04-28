7aaf820c1f9a7e077d5bbd5ed166f06d725e5d13
d.suffritti <d.suffritti@comarautomazioni.it>
2016-04-28 16:15:33 +0200

    Dump 20160428

---
+++ mysql_bkp/Dump20160428/gualini_posizione_cassetti.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `gualini` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gualini`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gualini
+-- ------------------------------------------------------
+-- Server version	5.7.9-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posizione_cassetti`
+--
+
+DROP TABLE IF EXISTS `posizione_cassetti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posizione_cassetti` (
+  `ID_POSIZIONE_CASSETTI` int(11) NOT NULL,
+  `DESCRIZIONE` varchar(15) NOT NULL DEFAULT 'UNDEF' COMMENT 'Indica se il cassetto è all''interno del magazzino, sull''estrattore di R1, di R2 oppure sull''estratture manuale:\nMAG, ES_R1, ES_R2, ES_MAN',
+  PRIMARY KEY (`ID_POSIZIONE_CASSETTI`),
+  UNIQUE KEY `DESCRIZIONE_UNIQUE` (`DESCRIZIONE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posizione_cassetti`
+--
+
+LOCK TABLES `posizione_cassetti` WRITE;
+/*!40000 ALTER TABLE `posizione_cassetti` DISABLE KEYS */;
+INSERT INTO `posizione_cassetti` VALUES (3,'ES_MAN'),(1,'ES_R1'),(2,'ES_R2'),(0,'MAG');
+/*!40000 ALTER TABLE `posizione_cassetti` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-28 16:14:44
